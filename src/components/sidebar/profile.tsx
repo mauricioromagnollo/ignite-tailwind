e33b99fc09e564e1a4c2d21a55871f0acd920909
mauricioromagnollo <mauricioromagnollo@gmail.com>
2023-11-19 02:53:52 -0300

    feat: add Sidebar component

---
+++ src/components/sidebar/profile.tsx
@@ -0,0 +1,29 @@
+import { LogOut } from 'lucide-react'
+import Image from 'next/image'
+
+export function Profile() {
+  return (
+    <div className="grid-cols-profile grid gap-3">
+      <Image
+        src="https://github.com/mauricioromagnollo.png"
+        className="h-10 w-10 rounded-full"
+        alt="Profile Image"
+        width={40}
+        height={40}
+      />
+
+      <div className="flex flex-1 flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">
+          Maurício Romagnollo
+        </span>
+        <span className="truncate text-sm text-zinc-500">
+          mauricioromagnollo@gmail.com
+        </span>
+      </div>
+
+      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  )
+}
---
+++ src/components/sidebar/sidebar.tsx
@@ -0,0 +1,54 @@
+import {
+  Search,
+  BarChart,
+  CheckSquare,
+  Flag,
+  Home,
+  SquareStack,
+  Users,
+  LifeBuoy,
+  Cog,
+} from 'lucide-react'
+
+import { Logo } from './logo'
+import { NavItem } from './nav-item'
+import { UsedSpaceWidget } from './used-space-widget'
+import { Profile } from './profile'
+import { Input } from '@/components/input'
+
+export function Sidebar() {
+  return (
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
+      <Logo />
+
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
+
+      <nav className="space-y-0.5">
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </nav>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+
+        <UsedSpaceWidget />
+
+        <div className="h-px bg-zinc-200"></div>
+
+        <Profile />
+      </div>
+    </aside>
+  )
+}
