53442d3d27df4c5efdae856c90ac433b13e3c40c
mauricioromagnollo <mauricioromagnollo@gmail.com>
2023-11-19 02:53:36 -0300

    feat: add Input component

---
+++ src/components/input.tsx
@@ -0,0 +1,35 @@
+import { ComponentProps } from 'react'
+
+type InputPrefixProps = ComponentProps<'div'>
+
+function Prefix(props: InputPrefixProps) {
+  return <div {...props} />
+}
+
+type InputControlProps = ComponentProps<'input'>
+
+function Control(props: InputControlProps) {
+  return (
+    <input
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      {...props}
+    />
+  )
+}
+
+export type InputRootPros = ComponentProps<'div'>
+
+function Root(props: InputRootPros) {
+  return (
+    <div
+      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      {...props}
+    />
+  )
+}
+
+export const Input = {
+  Root,
+  Prefix,
+  Control,
+}
