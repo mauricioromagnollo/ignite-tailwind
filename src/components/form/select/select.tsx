060633a97c26ea5b4d3c431e560ba59c5f14c984
mauricioromagnollo <mauricioromagnollo@gmail.com>
2023-11-20 15:26:58 -0300

    feat: add focus and animations

--- src/components/form/select/select.tsx
+++ src/components/form/select/select.tsx
@@ -12,7 +12,7 @@ export interface SelectProps extends RadixSelect.SelectProps {
 export function Select({ children, placeholder, ...props }: SelectProps) {
   return (
     <RadixSelect.Root {...props}>
-      <RadixSelect.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm data-[placeholder]:text-zinc-600">
+      <RadixSelect.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300 focus:ring-4 focus:ring-violet-100 data-[placeholder]:text-zinc-600">
         <RadixSelect.Value placeholder={placeholder} className="text-black" />
         <RadixSelect.Icon>
           <ChevronDown className="h-5 w-5 text-zinc-500" />
@@ -24,7 +24,7 @@ export function Select({ children, placeholder, ...props }: SelectProps) {
           side="bottom"
           position="popper"
           sideOffset={8}
-          className="z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
+          className="animate-slideDownAndFade z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
         >
           <RadixSelect.Viewport>{children}</RadixSelect.Viewport>
         </RadixSelect.Content>
